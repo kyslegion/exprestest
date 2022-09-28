4fb296631852f85045aacd1d203a366b76a261a5
Biron Christopher <96913467+kyslegion@users.noreply.github.com>
2022-09-28 16:30:27 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,14 @@
-const express = require('express');
+// Import packages
+const express = require("express");
+const home = require("./routes/home");
+
+// Middlewares
 const app = express();
-const path =require('path');
+app.use(express.json());
 
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.urlencoded({ extended: false }));
-app.use(express.static(__dirname+ '/'));
+// Routes
+app.use("/home", home);
 
-app.get('/',(req,res)=>{
-    console.log(req.route.path);
-    res.sendFile(path.join(__dirname, './connexion.html'));
-});
-// console.log(x.route);
-let server=app.listen(3000, ()=>{
-    console.log("Serveur localhost:", server.address().port);
-});
+// connection
+const port = process.env.PORT || 9001;
+app.listen(port, () => console.log(`Listening to port ${port}`));
